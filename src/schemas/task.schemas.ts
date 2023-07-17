2b08eb7cde0df88ff9a87226b8c6ab0f7256491e
Dasun Sathsara <me@dasunsathsara.com>
2023-07-17 21:33:05 +0530

    Implement user and task schemas completely

---
+++ src/schemas/task.schemas.ts
@@ -0,0 +1,61 @@
+import { buildJsonSchemas } from 'fastify-zod';
+import { z } from 'zod';
+
+const addTaskSchema = z.object({
+	description: z.string({
+		required_error: 'Description is required',
+		invalid_type_error: 'Description must be a string',
+	}),
+	completed: z.boolean({ invalid_type_error: 'Completed must be a boolean' }).optional(),
+});
+
+const updateTaskSchema = z.object({
+	description: z
+		.string({
+			invalid_type_error: 'Description must be a string',
+		})
+		.optional(),
+	completed: z.boolean({ invalid_type_error: 'Completed must be a boolean' }).optional(),
+});
+
+const taskIdSchema = z.object({
+	id: z.string({ required_error: 'ID is required', invalid_type_error: 'ID must be a string' }),
+});
+
+const findTaskSchema = z
+	.object({
+		completed: z.boolean({ invalid_type_error: 'Completed must be a boolean' }).optional(),
+		limit: z.number({ invalid_type_error: 'Limit must be a integer' }).optional(),
+		skip: z.number({ invalid_type_error: 'Skip must be a integer' }).optional(),
+		soryBy: z.string({ invalid_type_error: 'SortBy must be a string' }).optional(),
+	})
+	.refine(
+		data => {
+			// returns true when either limit is not specified or limit and skip both are specified
+			return data.limit === undefined || data.skip !== undefined;
+		},
+		{ message: 'Skip is required when limit is specified', path: ['skip'] },
+	)
+	.refine(
+		data => {
+			if (data.soryBy?.includes(':')) {
+				const [column, order] = data.soryBy.split(':');
+				return column! in ['description', 'createdAt', 'updatedAt'] && order! in ['asc', 'desc'];
+			} else {
+				return false;
+			}
+		},
+		{ message: 'SoryBy should have the following structure: <field_name>:[asc|desc]', path: ['sortBy'] },
+	);
+
+type AddTaskSchema = z.infer<typeof addTaskSchema>;
+type UpdateTaskSchema = z.infer<typeof updateTaskSchema>;
+
+const { schemas: taskSchemas, $ref } = buildJsonSchemas({
+	addTaskSchema,
+	taskIdSchema,
+	findTaskSchema,
+	updateTaskSchema,
+});
+
+export { AddTaskSchema, UpdateTaskSchema, taskSchemas, $ref };
--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -9,13 +9,45 @@ const userSchema = {
 
 const addUserSchema = z.object({
 	...userSchema,
-	password: z.string({ required_error: 'Password is required', invalid_type_error: 'Password must be a string' }),
+	password: z.string({
+		required_error: 'Password is required',
+		invalid_type_error: 'Password must be a string',
+	}),
 });
 
-type AddUserInput = z.infer<typeof addUserSchema>;
+const loginSchema = z.object({
+	email: z.string({ required_error: 'Email is required', invalid_type_error: 'Email must be a string' }).email(),
+	password: z.string({
+		required_error: 'Password is required',
+		invalid_type_error: 'Password must be a string',
+	}),
+});
+
+const updateUserSchema = z.object({
+	name: z.string({ required_error: 'Name is required', invalid_type_error: 'Name must be a string' }).optional(),
+	email: z
+		.string({ required_error: 'Email is required', invalid_type_error: 'Email must be a string' })
+		.email()
+		.optional(),
+	age: z.number({ invalid_type_error: 'Age must be an integer' }).optional(),
+	password: z
+		.string({ required_error: 'Password is required', invalid_type_error: 'Password must be a string' })
+		.optional(),
+});
+
+const getAvatarSchema = z.object({
+	id: z.string({ required_error: 'ID is required', invalid_type_error: 'ID must be a string' }),
+});
+
+type AddUserSchema = z.infer<typeof addUserSchema>;
+type LoginSchema = z.infer<typeof loginSchema>;
+type UpdateUserSchema = z.infer<typeof updateUserSchema>;
 
 const { schemas: userSchemas, $ref } = buildJsonSchemas({
 	addUserSchema,
+	loginSchema,
+	updateUserSchema,
+	getAvatarSchema,
 });
 
-export { AddUserInput, userSchemas, $ref };
+export { AddUserSchema, LoginSchema, UpdateUserSchema, userSchemas, $ref };
